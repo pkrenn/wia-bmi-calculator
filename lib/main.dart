bcd55436af818e72734b025a2ca069f2991ae5fb
pkrenn <paul.krenn@lab73.at>
2023-06-15 14:34:26 +0200

    Removed Theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
-      ),
       home: InputPage(),
     );
   }
