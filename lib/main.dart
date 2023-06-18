b60562ae84664cff0419b34535b8c99116dec52e
pkrenn <paul.krenn@lab73.at>
2023-06-18 19:30:29 +0200

    Removed Android and iOS Folders (to simplify the project) and added firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'screens/input_page.dart';
 
-void main() => runApp(BMICalculator());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(BMICalculator());
+}
 
 class BMICalculator extends StatelessWidget {
   @override
