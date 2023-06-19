49e1063344f6861bd3007fc8af2f9624f986ecc8
pkrenn <paul.krenn@lab73.at>
2023-06-19 10:40:48 +0200

    Removed Result page

--- lib/screens/result_page.dart
+++ lib/screens/result_page.dart
@@ -13,66 +13,8 @@ class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-        backgroundColor: kTapCardColour,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(15),
-              alignment: Alignment.bottomLeft,
-              child: Text(
-                'Your Result',
-                style: kTitleTextStyle,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-             child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    resultText.toUpperCase(),
-                    style: kResultTextStyle,
-                  ),
-                  Text(
-                    bmiResult,
-                    style: kBMIstyle,
-                  ),
-                  Text(
-                    interpretation,
-                    style: kBodyStyle,
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                  child: BottomButton(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    buttonTitle: 'Re-Calculate',
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+      body: Center(
+          child: Text("Calculated!")
       ),
     );
   }
